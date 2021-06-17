73c4aa92c45e13a7b89ea565cffefafd063d55c7
Jose Miguel Meilan Maldonado <josemeilan242@gmail.com>
2021-06-17 19:32:36 +0200

    Refactored code to include  a factory for dependencies

--- DependencyInversionPrincipleDemo/Program.cs
+++ DependencyInversionPrincipleDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 
 namespace DependencyInversionPrincipleDemo
 {
@@ -6,19 +7,18 @@ namespace DependencyInversionPrincipleDemo
     {
         static void Main(string[] args)
         {
-            Person owner = new Person
-            {
-                FirstName = "Tim",
-                LastName = "Corey",
-                EmailAddress = "tim@iamtimcorey.com",
-                PhoneNumber = "555-1212"
-            };
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Tim";
+            owner.LastName = "Corey";
+            owner.EmailAddress = "tim@iamtimcorey.com";
+            owner.PhoneNumber = "555-1212";
 
-            Chore chore = new Chore
-            {
-                ChoreName = "Take out the trash",
-                Owner = owner
-            };
+
+            IChore chore = Factory.CreateChore();
+
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+            
 
             chore.PerformedWork(3);
             chore.PerformedWork(1.5);
