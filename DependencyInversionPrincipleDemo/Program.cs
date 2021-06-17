a2b617d3a2ac67957f07425b87bdb7764b9d48fe
Jose Miguel Meilan Maldonado <josemeilan242@gmail.com>
2021-06-17 18:55:31 +0200

    Add project files.

---
+++ DependencyInversionPrincipleDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DependencyInversionPrincipleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person owner = new Person
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                EmailAddress = "tim@iamtimcorey.com",
+                PhoneNumber = "555-1212"
+            };
+
+            Chore chore = new Chore
+            {
+                ChoreName = "Take out the trash",
+                Owner = owner
+            };
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
